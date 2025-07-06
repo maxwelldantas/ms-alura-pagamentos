04f19f642034e8ace0562e392432a4df443e4017
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-05 21:50:24 -0300

    Projeto iniciado

---
+++ src/test/java/com/github/maxwelldantas/pagamentos/PagamentosApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.maxwelldantas.pagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+		throw new UnsupportedOperationException("Unimplemented method 'contextLoads'");
+	}
+
+}
