7d5896c2b0dcfd230ea51d6e120e4934013401a8
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-07-06 11:57:35 -0300

    Adicionado controller e service

---
+++ src/main/java/com/github/maxwelldantas/pagamentos/controller/PagamentoController.java
@@ -0,0 +1,64 @@
+package com.github.maxwelldantas.pagamentos.controller;
+
+import com.github.maxwelldantas.pagamentos.dto.PagamentoDTO;
+import com.github.maxwelldantas.pagamentos.service.PagamentoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/pagamentos")
+@RequiredArgsConstructor
+public class PagamentoController {
+
+	private final PagamentoService pagamentoService;
+
+	@PostMapping
+	public ResponseEntity<PagamentoDTO> cadastrar(@RequestBody @Valid PagamentoDTO dto, UriComponentsBuilder uriBuilder) {
+		var pagamento = pagamentoService.criarPagamento(dto);
+		URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
+
+		return ResponseEntity.created(endereco).body(pagamento);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<PagamentoDTO> atualizar(@PathVariable @NotNull Long id, @RequestBody @Valid PagamentoDTO dto) {
+		var atualizado = pagamentoService.atualizarPagamento(id, dto);
+
+		return ResponseEntity.ok(atualizado);
+	}
+
+	@GetMapping
+	public Page<PagamentoDTO> listar(@PageableDefault Pageable pageable) {
+		return pagamentoService.obterTodos(pageable);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<PagamentoDTO> detalhar(@PathVariable @NotNull Long id) {
+		var dto = pagamentoService.obterPorId(id);
+
+		return ResponseEntity.ok(dto);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<PagamentoDTO> remover(@PathVariable @NotNull Long id) {
+		pagamentoService.excluirPagamento(id);
+
+		return ResponseEntity.noContent().build();
+	}
+}
---
+++ src/main/java/com/github/maxwelldantas/pagamentos/service/PagamentoService.java
@@ -0,0 +1,62 @@
+package com.github.maxwelldantas.pagamentos.service;
+
+import com.github.maxwelldantas.pagamentos.dto.PagamentoDTO;
+import com.github.maxwelldantas.pagamentos.model.Pagamento;
+import com.github.maxwelldantas.pagamentos.model.Status;
+import com.github.maxwelldantas.pagamentos.repository.PagamentoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PagamentoService {
+
+	private final PagamentoRepository pagamentoRepository;
+	private final ModelMapper modelMapper;
+
+	public Page<PagamentoDTO> obterTodos(Pageable pageable) {
+		return pagamentoRepository
+				.findAll(pageable)
+				.map(pagamento -> modelMapper.map(pagamento, PagamentoDTO.class));
+	}
+
+	public PagamentoDTO obterPorId(Long id) {
+		var pagamento = pagamentoRepository.findById(id)
+				.orElseThrow(EntityNotFoundException::new);
+
+		return modelMapper.map(pagamento, PagamentoDTO.class);
+	}
+
+	@Transactional
+	public PagamentoDTO criarPagamento(PagamentoDTO pagamentoDTO) {
+		var pagamento = modelMapper.map(pagamentoDTO, Pagamento.class);
+		pagamento.setStatus(Status.CRIADO);
+
+		pagamentoRepository.save(pagamento);
+
+		return modelMapper.map(pagamento, PagamentoDTO.class);
+	}
+
+	@Transactional
+	public PagamentoDTO atualizarPagamento(Long id, PagamentoDTO pagamentoDTO) {
+		pagamentoRepository.findById(id)
+				.orElseThrow(EntityNotFoundException::new);
+
+		var pagamento = modelMapper.map(pagamentoDTO, Pagamento.class);
+		pagamento.setId(id);
+
+		pagamento = pagamentoRepository.save(pagamento);
+
+		return modelMapper.map(pagamento, PagamentoDTO.class);
+	}
+
+	@Transactional
+	public void excluirPagamento(Long id) {
+		pagamentoRepository.deleteById(id);
+	}
+}
